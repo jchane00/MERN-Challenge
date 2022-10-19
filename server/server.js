1d571237258b65033c361c9292a1c57096e8425c
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-10-18 21:04:23 -0500

    setup Apollo server

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit:'5000kb'}));
+
+// Route to Homepage
+app.use(express.static(path.join(__dirname, '../client/build')));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app, bodyParserConfig:'10mb' });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+  
+
+
+
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
